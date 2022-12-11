bd808b409b2be24e1ce8a4f9b94f0c067aa1c971
Yassin Hammami <yas.hammami@gmail.com>
2022-12-11 17:09:30 +0100

    J'attaque le javascript

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+
+const apiValue =  "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}"
+
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const errorMsg = document.querySelector('.error-msg')
+
+function handleSubmit(e) {
+e.preventDefault()
+
+if(!input.value === "") {
+    errorMsg.textContent="Je pense que tu t'es trompé, remplissez le champ s'il vous plait "
+    return;
+}
+}
+
+form.addEventListener("submit", handleSubmit)
